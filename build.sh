328ea594bbca4ab975d7618ed25336c967b9250b
Patrik Gäfvert <patrik@gafvert.se>
2023-10-19 21:11:02 +0200

    Add build file

---
+++ build.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+git clone --depth=1 https://github.com/raysan5/raylib.git
+cd raylib
+#cmake . -DGRAPHICS=GRAPHICS_API_OPENGL_21 -DCMAKE_C_FLAGS="-s" -DCMAKE_C_FLAGS_RELEASE="-s" -DBUILD_EXAMPLES="OFF"
+cmake . -DBUILD_EXAMPLES="OFF"
+make
+cp raylib/libraylib.a ../
+cp raylib/include/raylib.h ../
+cd ../
+gcc -s -o timer timer.c -I. -L. -lraylib -lm
+./timer
