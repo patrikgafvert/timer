af4e959b5246d495bbc56972993e1569729636f3
Patrik Gäfvert <patrik@gafvert.se>
2023-10-20 21:34:01 +0200

    Change so gl version detetion to 2.1

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #/bin/bash
 
-if [ ! -d "raylib" ]; then
+if [[ ! -d "raylib" ]]; then
 	git clone --depth=1 https://github.com/raysan5/raylib.git
 	cd raylib
-	#cmake . -DGRAPHICS=GRAPHICS_API_OPENGL_21 -DCMAKE_C_FLAGS="-s" -DCMAKE_C_FLAGS_RELEASE="-s" -DBUILD_EXAMPLES="OFF"
-	cmake . -DBUILD_EXAMPLES="OFF"
+	if [[ "$(glxinfo | grep "OpenGL version" | cut -d' ' -f4)" = "2.1" ]]; then
+		cmake . -DGRAPHICS=GRAPHICS_API_OPENGL_21 -DBUILD_EXAMPLES="OFF"
+	else
+		cmake . -DBUILD_EXAMPLES="OFF"
+	fi
 	make
 	cp raylib/libraylib.a ../
 	cp raylib/include/raylib.h ../
