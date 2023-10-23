c48054deea9addd7b75d19cc86552cf9041e6362
Patrik Gäfvert <patrik@gafvert.se>
2023-10-23 22:55:21 +0200

    Add windows build

---
+++ winbuild.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+if [[ -d "raylib" ]]; then
+	#git clone --depth=1 git@github.com:raysan5/raylib.git
+	cd raylib/src
+	make clean
+	for file in $(ls *.c);do x86_64-w64-mingw32-gcc -I external/glfw/include/ -DPLATFORM_DESKTOP -c $file;done
+	ar crs libraylib.a *.o	
+	cp libraylib.a ../../
+	cp raylib.h ../../
+	cd ../../
+fi
+x86_64-w64-mingw32-gcc -s -o timer timer.c -I. -L. -lraylib -lgdi32 -lwinmm 
+upx timer.exe
