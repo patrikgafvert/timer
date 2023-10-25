24f8eb6ac131841698663b681a7a589593867865
Patrik Gäfvert <patrik@gafvert.se>
2023-10-25 14:35:29 +0200

    Add new feature

--- newbuild.sh
+++ newbuild.sh
@@ -1,23 +1,28 @@
 #/bin/bash
 
-if [[ "$(glxinfo | grep 'OpenGL version string' | cut -f4 -d' ')" == "1.1" ]];then OPENGLV=GRAPHICS_API_OPENGL_11; fi 
-if [[ "$(glxinfo | grep 'OpenGL version string' | cut -f4 -d' ')" == "2.1" ]];then OPENGLV=GRAPHICS_API_OPENGL_21; fi 
-if [[ "$(glxinfo | grep 'OpenGL version string' | cut -f4 -d' ')" == "3.3" ]];then OPENGLV=GRAPHICS_API_OPENGL_33; fi 
-if [[ "$(glxinfo | grep 'OpenGL version string' | cut -f4 -d' ')" == "4.3" ]];then OPENGLV=GRAPHICS_API_OPENGL_43; fi 
+OPENGLV="$(glxinfo | grep 'OpenGL version string' | cut -f4 -d' ' | tr -d '.')"
+
+
 
 echo OpenGL Version $OPENGLV
 
-if [[ -d "raylib" ]]; then
-	#git clone --depth=1 git@github.com:raysan5/raylib.git
+if [[ ! -d "raylib" ]]; then
+	git clone --depth=1 git@github.com:raysan5/raylib.git
+fi
+if [[ ! -f "libraylib.a" ]]; then
 	cd raylib/src
 	rm -v *.o
-	for file in $(ls *.c);do gcc -I external/glfw/include/ -DPLATFORM_DESKTOP -D$OPENGLV -c $file;done
+	rm -v libraylib.a
+	#for file in $(ls *.c);do gcc -I external/glfw/include/ -DPLATFORM_DESKTOP -DGRAPHICS_API_OPENGL_33 -c $file;done
+	for file in $(ls *.c);do gcc -I external/glfw/include/ -DPLATFORM_DESKTOP -DGRAPHICS_API_OPENGL_$OPENGLV -c $file;done
 	ar crs libraylib.a *.o	
 	cp libraylib.a ../../
 	cp raylib.h ../../
 	cd ../../
 fi
 
-gcc -s -o timer timer.c -I. -L. -lraylib -lm && ./timer
+gcc -s -o timer timer.c -I. -L. -lraylib -lm
 
 upx --best ./timer
+
+./timer
--- winbuild.sh
+++ winbuild.sh
@@ -1,14 +1,19 @@
 #/bin/bash
 
-if [[ -d "raylib" ]]; then
-	#git clone --depth=1 git@github.com:raysan5/raylib.git
+if [[ ! -d "raylib" ]]; then
+	git clone --depth=1 git@github.com:raysan5/raylib.git
+fi
+
+if [[ ! -f "libwinraylib.a" ]]; then
 	cd raylib/src
 	rm -v *.o
+	rm -v libraylib.a
 	for file in $(ls *.c);do x86_64-w64-mingw32-gcc -I external/glfw/include/ -DPLATFORM_DESKTOP -c $file;done
 	ar crs libraylib.a *.o	
-	cp libraylib.a ../../
+	cp libraylib.a ../../libwinraylib.a
 	cp raylib.h ../../
 	cd ../../
 fi
-x86_64-w64-mingw32-gcc -s -o timer timer.c -I. -L. -lraylib -lgdi32 -lwinmm 
+
+x86_64-w64-mingw32-gcc -s -o timer timer.c -I. -L. -lwinraylib -lgdi32 -lwinmm 
 upx timer.exe
