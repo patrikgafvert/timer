964702368e4d67a781e5125333b6639f3d25c90d
Patrik Gäfvert <patrik@gafvert.se>
2023-10-28 13:01:39 +0200

    Change the lib name

--- winbuild.sh
+++ winbuild.sh
@@ -3,21 +3,23 @@
 xc_prefix="x86_64-w64-mingw32-"
 cc="${xc_prefix}gcc"
 
+echo Compiling with $cc
+
 if [[ ! -d "raylib" ]]; then
 	git clone --depth=1 git@github.com:raysan5/raylib.git
 fi
 
-if [[ ! -f "libwinraylib.a" ]]; then
+if [[ ! -f "libraylib-win.a" ]]; then
 	cd raylib/src
 	git pull
 	rm -v *.o
 	rm -v libraylib.a
-	for file in $(ls *.c);do $cc -I external/glfw/include/ -DPLATFORM_DESKTOP -c $file;done
+	for file in $(ls *.c);do $cc -I /usr/x86_64-w64-mingw32/include/ -I external/glfw/include/ -DPLATFORM_DESKTOP -c $file;done
 	${xc_prefix}ar crs libraylib.a *.o	
-	cp libraylib.a ../../libwinraylib.a
+	cp libraylib.a ../../libraylib-win.a
 	cp raylib.h ../../
 	cd ../../
 fi
 
-$cc -s -o timer timer.c -I. -L. -lwinraylib -lgdi32 -lwinmm 
-upx timer.exe
+$cc -s -o timer timer.c -I. -L. -lraylib-win -lgdi32 -lwinmm
+upx --best timer.exe
