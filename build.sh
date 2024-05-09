2c2812011ad6c8c2f7603c4f96241a84a5a133a0
Patrik Gäfvert <patrik@arch.localdomain>
2024-05-09 13:41:10 +0200

    Changed warnings

--- build.sh
+++ build.sh
@@ -14,14 +14,14 @@ if [[ ! -f "libraylib.a" ]]; then
 	cd src
 	rm -v *.o
 	rm -v libraylib.a
-	for file in $(ls *.c);do gcc -Iexternal/glfw/include -DPLATFORM_DESKTOP -DGRAPHICS_API_OPENGL_$OPENGLV -D_GLFW_X11 -c $file;done
+	for file in $(ls *.c);do gcc -Iexternal/glfw/include -DPLATFORM_DESKTOP -DGRAPHICS_API_OPENGL_$OPENGLV -D_GLFW_X11 -Wall -Wextra -c $file;done
 	ar crs libraylib.a *.o	
 	cp libraylib.a ../../
 	cp raylib.h ../../
 	cd ../../
 fi
 
-gcc -s -o timer timer.c -I. -L. -lraylib -lm
+gcc -s -o timer timer.c -I. -L. -lraylib -lm -Wall -Wextra
 
 upx --best ./timer
 
